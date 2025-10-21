000d8e40c77420461e6e585ac86380f10ab395e6
bmilici <brad.milici@yahoo.com>
2025-10-21 16:00:12 -0400

    Updated favicon for Dipsgiving

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Dipsgiving",
   description: "The official Dipsgiving website – see you November 2025!",
+  icons: {
+  icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
