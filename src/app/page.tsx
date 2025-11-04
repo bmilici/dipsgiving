bd345c50c2e6a5daf9ef7efecf80e1bcab9ea90a
bmilici <brad.milici@yahoo.com>
2025-11-03 20:35:51 -0500

    Updating words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ export default function Page() {
             onClick={() => registerRef.current?.open(true)}
             className="rounded-xl bg-amber-100 px-8 py-3 text-lg font-semibold text-orange-800 shadow-sm hover:bg-amber-200 transition"
           >
-            Already RSVP’d? Register Dip
+            Register Dip(Already RSVP’d)
           </button>
         </div>
       </section>
